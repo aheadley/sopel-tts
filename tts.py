00561bc2523baec5d5f8c9ff29e12a787340886c
aheadley <aheadley@nexcess.net>
2016-12-05 18:11:54 -0500

    split speech synth and playing into separate processes, and autodetect language

--- tts.py
+++ tts.py
@@ -13,6 +13,8 @@ import urlparse
 import re
 import multiprocessing
 import glob
+import time
+from xml.sax.saxutils import escape as xml_escape
 
 import sopel.module
 import sopel.tools
@@ -23,6 +25,16 @@ import boto3
 
 VALID_AUDIO_FORMATS         = ['mp3', 'ogg_vorbis', 'pcm']
 VALID_SAMPLE_RATES          = ['8000', '16000', '22050']
+VALID_VOICE_PITCHES         = ['low', 'medium', 'high']
+MESSAGE_TEMPLATE            = u"""
+<speak><prosody rate="{rate}" pitch="{pitch}">{message}</prosody></speak>
+""".strip()
+PHONEME_TEMPLATE            = u'<phoneme ph="{}">{}</phoneme>'
+EMPHASIS_TEMPLATE           = u'<emphasis level="{}">{}</emphasis>'
+TOKEN_REPLACEMENT_MAP       = {
+    r'lol':                      'ph:l o l',
+    r'\<3':                      'heart',
+}
 
 log = sopel.logger.get_logger('tts')
 log.setLevel(logging.DEBUG)
@@ -30,12 +42,13 @@ log.setLevel(logging.DEBUG)
 class TTSSection(StaticSection):
     access_key      = ValidatedAttribute('access_key', str)
     secret_key      = ValidatedAttribute('secret_key', str)
-    region          = ValidatedAttribute('region', str)
+    region          = ValidatedAttribute('region', str, default='us-east-1')
 
-    lang_family     = ValidatedAttribute('lang_family',
-        lambda lf: len(lf) == 2 and str(lf))
-    audio_format    = ChoiceAttribute('audio_format', VALID_AUDIO_FORMATS)
-    sample_rate     = ChoiceAttribute('sample_rate', VALID_SAMPLE_RATES)
+    default_lang    = ValidatedAttribute('default_lang',
+        lambda lf: len(lf) == 2 and str(lf), default='en')
+    audio_format    = ChoiceAttribute('audio_format', VALID_AUDIO_FORMATS, default='mp3')
+    sample_rate     = ChoiceAttribute('sample_rate', VALID_SAMPLE_RATES, default='22050')
+    speech_rate     = ValidatedAttribute('speech_rate', float, default=1.1)
 
     play_cmd        = ValidatedAttribute('play_cmd',
         lambda pc: ('{}' in str(pc) and str(pc)))
@@ -47,98 +60,130 @@ class TTSSection(StaticSection):
 
 def setup(bot):
     bot.config.define_section('tts', TTSSection)
-    polly_client = boto3.client('polly',
-        aws_access_key_id=bot.config.tts.access_key,
-        aws_secret_access_key=bot.config.tts.secret_key,
-    )
-    voices = sorted([v \
-        for v in polly_client.describe_voices()['Voices'] \
-        if v['LanguageCode'].startswith(bot.config.tts.lang_family + u'-')],
-        key=lambda v: v['Id'])
-    log.debug(u'Pulled %d voices: %s', len(voices), u', '.join(v['Id'] for v in voices))
 
     if not bot.memory.contains('tts'):
         bot.memory['tts'] = sopel.tools.SopelMemory()
 
-    bot.memory['tts']['polly_client'] = polly_client
-    bot.memory['tts']['voices'] = voices
-    bot.memory['tts']['speech_queue'] = multiprocessing.Queue()
-    bot.memory['tts']['speech_proc'] = multiprocessing.Process(target=worker_proc,
-        args=(bot.memory['tts']['speech_queue'], polly_client, bot.config.tts))
+    bot.memory['tts']['msg_queue'] = multiprocessing.Queue()
+    bot.memory['tts']['audio_queue'] = multiprocessing.Queue()
 
-    log.debug('Setup finished, starting audio worker and queueing debug message')
-    bot.memory['tts']['speech_proc'].start()
-    bot.memory['tts']['speech_queue'].put((
-        'All systems nominal, ready for messages.', nick2bucket(bot.nick, voices)))
+    bot.memory['tts']['worker_proc'] = multiprocessing.Process(target=worker_proc,
+        args=(bot.memory['tts']['msg_queue'], bot.memory['tts']['audio_queue'],
+            bot.config.tts))
+    bot.memory['tts']['player_proc'] = multiprocessing.Process(target=player_proc,
+        args=(bot.memory['tts']['audio_queue'], bot.config.tts.play_cmd))
+
+    log.debug('Setup finished, starting audio+player workers and queueing debug message')
+    bot.memory['tts']['worker_proc'].start()
+    bot.memory['tts']['player_proc'].start()
+
+    bot.memory['tts']['msg_queue'].put(('All systems nominal, ready for messages.', bot.nick))
 
 @sopel.module.rule(r'.*')
 def speak(bot, trigger):
-    if trigger.nick != bot.nick and str(trigger.nick) not in bot.config.tts.mute_nicks:
-        msg = clean_message(trigger.group(0))
-        if msg and not any(msg.startswith(c) for c in bot.config.tts.mute_msgs):
-            bot.memory['tts']['speech_queue'].put((
-                msg, nick2bucket(trigger.nick, bot.memory['tts']['voices'])))
-            log.debug(u'Queued message: "%s"', msg)
-        else:
-            log.warning(u'Skipping garbage message: "%s"', trigger.group(0))
+    original_msg = trigger.group(0).strip()
+    if trigger.nick != bot.nick \
+            and str(trigger.nick) not in bot.config.tts.mute_nicks \
+            and not any(original_msg.startswith(c) for c in bot.config.tts.mute_msgs):
+        bot.memory['tts']['msg_queue'].put((original_msg, trigger.nick))
+        log.debug(u'Queued message: "%s"', original_msg)
     else:
-        log.info(u'Ignoring muted nick: %s', trigger.nick)
+        log.info(u'Ignoring message from: %s', trigger.nick)
 speak.priority = 'medium'
 
-@sopel.module.commands('myvoice')
-def show_my_voice(bot, trigger):
-    v = nick2bucket(trigger.nick, bot.memory['tts']['voices'])
-    bot.say(u'Your voice is: {name} ({lang_name} - {gender}) [{lang_code}]'.format(
-        name=v['Id'],
-        lang_name=v['LanguageName'],
-        gender=v['Gender'],
-        lang_code=v['LanguageCode'],
-    ))
-show_my_voice.priority = 'medium'
-
-def worker_proc(queue, polly_client, tts_config):
+def worker_proc(msg_queue, audio_queue, tts_config):
     logging.basicConfig()
     log = logging.getLogger('sopel.modules.tts.worker-{:05d}'.format(os.getpid()))
     log.setLevel(logging.DEBUG)
 
-    log.debug('Speech worker process starting...')
+    log.debug('Worker process starting')
+
+    import langid
+    # there's a large delay while langid loads the model on the first classify()
+    # call, so we do that now
+    langid.classify('default text')
 
     for old_fn in glob.glob(os.path.join(tempfile.gettempdir(), 'sopel-tts-*.*')):
         log.info('Deleteing old temp file: %s', old_fn)
         os.unlink(old_fn)
 
-    keep_running = True
+    polly_client = boto3.client('polly',
+        aws_access_key_id=tts_config.access_key,
+        aws_secret_access_key=tts_config.secret_key,
+    )
+
+    voices = sorted(polly_client.describe_voices()['Voices'], key=lambda v: v['Id'])
+    voice_langs = set(v['LanguageCode'].split('-')[0] for v in voices)
+    log.debug(u'Found %d voices from language families: %s',
+        len(voices), u', '.join(voice_langs))
+
+    log.info('Ready to accept messages')
+    while True:
+        orig_msg, nick = msg_queue.get()
+
+        msg = clean_message(orig_msg)
+        if not msg:
+            log.warning(u'Skipping garbage message: "%s"', msg)
+            continue
+        msg = MESSAGE_TEMPLATE.format(
+            rate=tts_config.speech_rate,
+            pitch=nick2bucket(nick, VALID_VOICE_PITCHES),
+            message=msg,
+        )
+
+        msg_lang = langid.classify(orig_msg)[0]
+        log.debug(u'Detected message language as: %s', msg_lang)
+        if msg_lang not in voice_langs:
+            msg_lang = tts_config.default_lang
+            log.warning(u'Detected language is not an available voice, defaulting to: %s', msg_lang)
+        msg_voices = [v for v in voices if v['LanguageCode'].startswith(msg_lang + '-')]
+        voice = nick2bucket(nick, msg_voices)
+
+        log.info(u'Synthesizing with "%s" [%s@%s]: "%s"',
+            voice['Id'], tts_config.audio_format, tts_config.sample_rate, msg)
+        resp = polly_client.synthesize_speech(
+            Text=msg,
+            TextType='ssml',
+            VoiceId=voice['Id'],
+            OutputFormat=tts_config.audio_format,
+            SampleRate=tts_config.sample_rate,
+        )
+
+        tmp_f, tmp_fn = tempfile.mkstemp(
+            suffix='.' + tts_config.audio_format,
+            prefix='sopel-tts-',
+        )
+        os.close(tmp_f)
+        log.debug('Writing audio to: %s', tmp_fn)
+        with open(tmp_fn, 'w') as tmp_f:
+            try:
+                tmp_f.write(resp['AudioStream'].read())
+            except Exception as err:
+                log.error('Failed to speak: %s', err)
+                log.exception(err)
+                continue
+        audio_queue.put(tmp_fn)
+        log.debug('Queued file: %s', tmp_fn)
+
+    log.debug('Worker process stopping')
+
+def player_proc(audio_queue, play_cmd):
+    logging.basicConfig()
+    log = logging.getLogger('sopel.modules.tts.player-{:05d}'.format(os.getpid()))
+    log.setLevel(logging.DEBUG)
+
+    log.debug('Player process starting')
+
     with open('/dev/null', 'rw') as dev_null:
-        log.info('Ready to accept messages!')
-        while keep_running:
-            msg, voice = queue.get()
+        log.info('Ready to accept audio files')
+        while True:
+            fn = audio_queue.get()
+            log.debug('Playing queued audio from file: %s', fn)
+            subprocess.call(play_cmd.format(fn).split(), stdout=dev_null, stderr=dev_null)
+            os.unlink(fn)
+            time.sleep(0.3)
+    log.debug('Player process stopping')
 
-            log.info(u'Synthesizing with "%s" [%s@%s]: "%s"',
-                voice['Id'], tts_config.audio_format, tts_config.sample_rate, msg)
-            resp = polly_client.synthesize_speech(
-                Text=msg,
-                VoiceId=voice['Id'],
-                OutputFormat=tts_config.audio_format,
-                SampleRate=tts_config.sample_rate,
-            )
-
-            tmp_f, tmp_fn = tempfile.mkstemp(
-                suffix='.' + tts_config.audio_format,
-                prefix='sopel-tts-',
-            )
-            os.close(tmp_f)
-            log.debug('Writing audio to: %s', tmp_fn)
-            with open(tmp_fn, 'w') as tmp_f:
-                try:
-                    tmp_f.write(resp['AudioStream'].read())
-                except Exception as err:
-                    log.error('Failed to speak: %s', err)
-                    log.exception(err)
-                    continue
-            log.debug('Playing queued audio from: %s', tmp_fn)
-            subprocess.call(tts_config.play_cmd.format(tmp_fn).split(), stdout=dev_null, stderr=dev_null)
-            os.unlink(tmp_fn)
-    log.debug('Speech worker process exiting!')
 
 def nick2bucket(nick, buckets):
     return buckets[int(nick.strip().lower().encode('hex'), 16) % len(buckets)]
@@ -158,12 +203,21 @@ def clean_token(t):
                 t = u'.'.join(domain_parts[1:])
             else:
                 t = url.netloc
-            return u'{{ {} }}'.format(t)
+            return EMPHASIS_TEMPLATE.format('strong', t)
         except Exception:
             return u''
+    # fixup tokens that polly doesn't know how to pronounce
+    for rgx, ph in TOKEN_REPLACEMENT_MAP.iteritems():
+        if re.match(rgx, t, re.I):
+            if ph is None:
+                return u''
+            elif ph.startswith('ph:'):
+                return PHONEME_TEMPLATE.format(ph[3:], t)
+            else:
+                return ph
     # reduce characters repeated 4+ times in a row to 2 characters
     t = re.sub(r'((.)\2)(\2{2,})', r'\1', t)
-    if not t.encode('ascii', 'ignore').strip():
-        # filter out unicode garbage, unfortunately this sucks for other languages
-        t = u''
-    return t.strip()
+    t = xml_escape(t.strip())
+    if t.startswith('*') and t.endswith('*') and len(t) >= 3:
+        t = EMPHASIS_TEMPLATE.format('strong', t[1:-1])
+    return t
